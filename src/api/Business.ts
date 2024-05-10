d5f6628940b0fc1215ce5dd218f2c8dc377132cd
yuliussmayoru <yuliussmayoru@gmail.com>
2024-05-10 14:52:55 +0700

    update fixing api datatype

--- src/api/Business.ts
+++ src/api/Business.ts
@@ -1,3 +1,6 @@
+import { Image } from "./User";
+import { Product } from "./Product";
+
 export interface Business {
   business_types: string;
   business_name: string;
@@ -6,4 +9,38 @@ export interface Business {
   create_at: Date;
   update_at: Date;
   description: string;
+}
+
+export interface BusinessPostEditPayload {
+  business_name: string;
+  business_types: string;
+  description: string;
+}
+
+export interface BusinessPostEditDetailResponse {
+  business_types: string;
+  business_name: string;
+  profile_url: string;
+  id: string;
+  create_at: Date;
+  update_at: Date;
+  description: string;
+  user_phone_number: string;
+  user_email: string;
+  username: string;
+  product: Product[];
+  business_images: Image[];
+}
+
+export interface AllBusiness {
+  business_name: string;
+  business_types: string;
+  create_at: Date;
+  description: string;
+  id: string;
+  profile_url: string
+  update_at: Date;
+  user_email: string;
+  user_phone_number: string;
+  username: string;
 }
--- src/api/User.ts
+++ src/api/User.ts
@@ -11,7 +11,27 @@ export interface UserPostEdit {
   email: string;
 }
 
-export interface getUserDetail extends User {
+export interface getUserDetail extends UserPostEdit {
+  id: string;
+  create_at: Date;
+  update_at: Date;
+  user_type: string;
+  profile_url: string;
+  images: Image[];
+  product_amount: number;
+  business_amount: number;
+  business: Business[];
+  product: Product[];
+}
+
+export interface UserEditResponse {
+  access_token(access_token: any): unknown;
+  phone_number: string;
+  address: string;
+  occupation: string;
+  description: string;
+  username: string;
+  email: string;
   id: string;
   create_at: Date;
   update_at: Date;
